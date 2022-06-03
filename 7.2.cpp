7a34239f7f03a574bbdbdbfd09963c65297ed1f5
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:16:31 +0300

    Create 7.2.cpp

---
+++ 7.2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int size = 4;
+    int numbers[size] = { 5, 112, 4, 3 };
+    for (int i = 0; i < size; i++) {
+        cout << numbers[size - i - 1] << " ";
+    }
+    
+
+    return 0;
+}
+
