59a8b9940fc8b6b15369d46c0a710af9f5a0d6de
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:15:31 +0300

    Create 3.5.cpp

---
+++ 3.5.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    double x, y;
+    cin >> x >> y;
+    if (x != y) {
+        cout << "This number is the largest: " << max(x, y) << "\n";
+        cout << "This number is the smallest: " << min(x, y);
+    }
+    
+    if (x == y) {
+
+        cout << "This numbers is equal";
+    }
+    return 0;
+}
+
