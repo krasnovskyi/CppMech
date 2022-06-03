af5f4375749cff5542c288b5c6b6bbbbd839664b
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:15:50 +0300

    Create 4.3 a).cpp

---
+++ 4.3 a).cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+double foo(int x, int n)
+{
+    double y = 0;
+    for (int i = n; i >= 0; i--)
+        y += pow(x, i);
+    return y;
+}
+
+int main()
+{
+    int x = 2, n = 3;
+    cout << "y = " << foo(x, n);
+
+    return 0;
+}
+
