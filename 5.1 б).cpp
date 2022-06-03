c05f520f1ec3b1ff0f68ede2a020e9a67ae9fcb7
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:16:14 +0300

    Create 5.1 б).cpp

---
+++ 5.1 б).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	double n;
+	cin >> n;
+	double a = 0;
+	for (double i = 1; i > 0; i++) {
+		double x = 1 / i;
+		if (x < n) {
+			a += x;
+			if (a > n) {
+				cout << i;
+				break;
+			}
+		}
+	}
+	return 0;
+}
+
