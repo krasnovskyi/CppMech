0e027f2d9e73738f6ab69adc81f781263b2979f3
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:15:15 +0300

    Create 3.1 a).cpp

---
+++ 3.1 a).cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string x, a, b, c;
+    cin >> x;
+    a = x[2];
+    b = x[1];
+    c = x[0];
+    cout << "Units:    " << a << "\n" << "Tens:     " << b << "\n" << "Hundreds: " << c;
+
+
+    return 0;
+}
+
