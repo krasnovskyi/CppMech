d0f1e22ae1e6d6faeafb706241fad0a36901ca22
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:13:49 +0300

    Create 1.7.cpp

---
+++ 1.7.cpp
@@ -0,0 +1,16 @@
+﻿
+
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+    double x, i, f;
+    printf("Enter the number:");
+    scanf_s("%lf", &x);
+    f = modf(x, &i);
+    printf("Fractional part of a number: %f\nInteger part of a number: %0.0f\n", f, i);
+
+    
+}
+
