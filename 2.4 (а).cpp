2c0f835fac952ce3990485ab57d69526c8cb1676
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:15:03 +0300

    Create 2.4 (а).cpp

---
+++ 2.4 (а).cpp
@@ -0,0 +1,18 @@
+﻿
+
+#include <iostream>
+#include <math.h>
+
+
+
+int main()
+{
+	double x, y;
+
+	printf("x = ");
+	scanf_s("%lf", &x);
+	y = pow((pow(x, 2) + 1), 2);
+	printf("y = %lf", y);
+	
+}
+
