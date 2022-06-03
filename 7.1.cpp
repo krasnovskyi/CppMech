33f14b2168368807b0c552aedd98aaa22ca520a8
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:16:28 +0300

    Create 7.1.cpp

---
+++ 7.1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    double x;
+    cout << "Enter x: ";
+    cin >> x;
+    int numbers[5] = { 1,5,10,4,6 };
+    for (int i = 0; i < 5; i++) {
+        if (numbers[i] < x) {
+            cout << numbers[i] << "\n";
+        }
+    }
+
+
+    return 0;
+}
+
