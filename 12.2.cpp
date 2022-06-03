660c016053ae75d4cf08ed598f47983a39fb30be
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:14:32 +0300

    Create 12.2.cpp

---
+++ 12.2.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+const int N = 10;
+
+int main2() {
+	char str[N];
+	unsigned long long sums = 0L;
+	unsigned long long x;
+	size_t err;
+	char* err2;
+	while (cin.get(str, 10)) {
+		
+		x = strtoull(str, &err2, N);
+		sums += x;
+	}
+	cout << "sum" << sums << "\n";
+	return 0;
+}
