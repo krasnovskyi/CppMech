6f3fb8b9b18036fff216a7100e4248ed10dd1239
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:15:42 +0300

    Create 4.1.cpp

---
+++ 4.1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+double foo(double x, int n)
+{
+    double y = 1;
+    
+    for (int i = 1; i <= n; i++)
+        y *= sin(x);
+        return y;
+
+    
+
+
+}
+
+
+int main()
+{
+    double x;
+    int n;
+    cout << "Enter x: \n";
+    cin >> x;
+    cout << "Enter n: ";
+    cin >> n;
+    cout << foo(x, n);
+
+    return 0;
+}
+
+
