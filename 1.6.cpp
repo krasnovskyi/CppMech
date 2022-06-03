b24f255327c5cc5f0232300651194ecbc81f8d0c
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:13:43 +0300

    Create 1.6.cpp

---
+++ 1.6.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+
+	double C, F;
+	printf("C = ");
+	scanf_s("%lf", &C);
+	F = (((C * 9) / 5) + 32);
+	printf("F = %0.0f\n", F);
+
+
+
+
+}
+
