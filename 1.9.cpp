556d10d51f99e0e66d9e47e0a52fcc42c61b7fe3
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:13:56 +0300

    Create 1.9.cpp

---
+++ 1.9.cpp
@@ -0,0 +1,23 @@
+﻿
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	double x, y, Harmonic, Arithmetic;
+	printf("Enter your numbers:   ");
+	scanf_s("%lf", &x);
+	scanf_s("%lf", &y);
+	Harmonic = 2 / ((1 / x) + (1 / y));
+	Arithmetic = (x + y) / 2;
+	printf("Arithmetic mean:   %lf, %ef\nHarmonic mean:   %lf, %ef\n", Arithmetic, Arithmetic, Harmonic, Arithmetic);
+
+
+
+
+
+}
+
+
