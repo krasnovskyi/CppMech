08d9711d8401993d071cfaec10b98cdbddea3547
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:15:36 +0300

    Create 3.7 a).cpp

---
+++ 3.7 a).cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+double Diskriminant(double a, double b, double c)
+{
+    return b * b - 4 * a * c;
+}
+
+int main()
+{
+    double a, b, c, x1, x2;
+    cout << "Enter a, b, c: ";
+    cin >> a >> b >> c;
+    if (Diskriminant(a, b, c) == 0) {
+
+        x1 = -b / 2 * a;
+        cout << "The equation has one root: " << x1;
+
+    }
+    else if (Diskriminant(a, b, c) < 0) {
+        cout << "The equation hasn't roots";
+    }
+    else if (Diskriminant(a, b, c) > 0) {
+        x1 = -b + sqrt(Diskriminant(a, b, c)) / 2 * a;
+        x2 = -b - sqrt(Diskriminant(a, b, c)) / 2 * a;
+        cout << "The equation has two roots: " << x1 << ", " << x2;
+    }
+
+
+    return 0;
+}
+
