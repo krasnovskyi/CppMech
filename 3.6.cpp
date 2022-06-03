8b3d04436181e6f0ca6a64d13365a0a782ab83c9
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:15:34 +0300

    Create 3.6.cpp

---
+++ 3.6.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+int main()
+{
+    double x, y, z;
+    cin >> x >> y >> z;
+    if (x != y && y != z && x != z) {
+        double m;
+        m = max(abs(x), abs(y));
+        if (m > abs(z)) {
+            cout << "The largest number is: " << m << "\n";
+        }
+        else {
+            cout << "The largest number is: " << z << "\n";
+        }
+        double n;
+        n = min(abs(x), abs(y));
+        if (n < abs(z)) {
+            cout << "The smallest number is: " << n << "\n";
+        }
+        else {
+            cout << "The smallest number is: " << z << "\n";
+        }
+
+    }
+    
+    
+    
+    
+    
+    return 0;
+}
+
