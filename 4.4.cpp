c38c31e0d47d1a34c53493e1a2dd8c6c9d6a11f4
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:15:56 +0300

    Create 4.4.cpp

---
+++ 4.4.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+double foo(double n, double x)
+{
+    double y = 0;
+    for (int i = 0; i <= n; ++i)
+        y += i * pow(x, i);
+    return y;
+}
+
+int main()
+{
+    double n, x;
+    cout << "Enter n: ";
+    cin >> n;
+    cout << "Enter x: ";
+    cin >> x;
+    cout << foo(n, x);
+    return 0;
+}
+
+
