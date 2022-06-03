68c2d0ccac45119cff2374c0b92230854bc04ea0
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:15:47 +0300

    Create 4.2.cpp

---
+++ 4.2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int grow(int n)
+{
+    int y = 1;
+    for (int i = 1; i <= n; ++i)
+        y *= i;
+    return y;
+}
+int decrease(int n) 
+{
+    int y = 1;
+    for (int i = n; i > 0; i--)
+        y *= i;
+    return y;
+
+}
+
+
+int main()
+{
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    cout << grow(n) << "\n";
+    cout << decrease(n);
+    return 0;
+}
+
