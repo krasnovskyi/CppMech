c293983993bfd0d319a0da464f2c5abd6771b084
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:15:29 +0300

    Create 3.3.cpp

---
+++ 3.3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    double a, b, c;
+    
+    cin >> a >> b >> c;
+    if ((a < pow(2, 10) && b < pow(2, 10) && c < pow(2, 10))) {
+
+        cout << a * b * c;
+
+    }
+    else {
+         
+        cout << " ";
+
+    }
+    
+    
+
+
+    return 0;
+}
+
