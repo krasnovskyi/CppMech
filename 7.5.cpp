10d740c4bd39257fcd448a7b8654b5ff77773e2c
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:16:38 +0300

    Create 7.5.cpp

---
+++ 7.5.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[50];
+    for (int i = 0; i < 50; i++) {
+
+            cout << "[" << i + 1 << "]" << ": ";
+            cin >> arr[i];
+            if (arr[i] == 0) {
+                break;
+            }
+    }
+    int a = 0;
+    int b = 0;
+    for (int i = 0; i < 50; i++) {
+        if (arr[i] == 0) {
+            break;
+
+        }
+        if (arr[i] % 2 == 0) {
+            a++;
+            
+        }
+        if (arr[i] % 2 != 0) {
+            b++;
+            
+        }
+    }
+    cout << "Odd numbers: " << b << " " << "\n";
+    cout << "Even numbers: " << a << " ";
+
+    return 0;
+}
