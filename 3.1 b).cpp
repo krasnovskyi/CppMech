7125aa3c5c9620945f101d92749d7d7178be35a9
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:15:18 +0300

    Create 3.1 b).cpp

---
+++ 3.1 b).cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int x, a, b, c;
+    cin >> x;
+    a = x%10;
+    b = (x%100)/10;
+    c = (x%1000)/100;
+    
+    cout << a + b + c;
+
+    return 0;
+}
