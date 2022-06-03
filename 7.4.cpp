69e17318cb42a9907b723fa39d9772e1777110fe
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:16:35 +0300

    Create 7.4.cpp

---
+++ 7.4.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    
+    const int size = 5;
+    int arr[size];
+    int max = arr[0];
+    for (int i = 0; i < size; i++) {
+        cout << "[" << i + 1 << "]" << ": ";
+        cin >> arr[i];
+        
+    }
+    
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+        
+    }
+
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    cout << "\n" << "Maximum element: " << max;
+    return 0;
+}
+
