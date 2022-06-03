b3c812a99eff486cb54c5c57b616c2cbe484e335
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:16:17 +0300

    Create 5.2.cpp

---
+++ 5.2.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int f(int n)
+{
+    int a = 0;
+    int b = 1;
+    for (int i = 0; i < n; i++)
+    {
+        a = a + b;
+        b = a - b;
+    }
+    return a;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << "Result: " << f(n) << "\n";
+   
+    return 0;
+}
+
+
