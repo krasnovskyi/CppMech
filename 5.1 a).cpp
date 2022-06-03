f5b5375bea4144a88dbfc0f2f7214d22acabae7c
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:16:12 +0300

    Create 5.1 a).cpp

---
+++ 5.1 a).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+	
+	double n;
+	cin >> n;
+	double a = 0;
+	for (double i = 1; i > 0; i++) {
+		double x = 1 / i;
+        if (x < n) {
+		    a += x;
+			if (a > n) {
+				cout << a;
+				break;
+			}
+		}
+	}
+	return 0;
+}
+
