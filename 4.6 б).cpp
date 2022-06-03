d08b219369095d0f5de435931deb9a7d16a2a48f
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:16:03 +0300

    Create 4.6 б).cpp

---
+++ 4.6 б).cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+double foo(int n) 
+{
+    double y = 0;
+
+    for (int i = 0; i <= n; ++i)
+        y += sqrt(3*i);
+    return y;
+
+}
+
+
+int main()
+{
+    double n;
+    cout << "Enter n: ";
+    cin >> n;
+    cout << foo(n);
+    return 0;
+}
+
+
