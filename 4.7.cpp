3ad02567154802e0a51c683e04a31ac0497db9f0
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:16:06 +0300

    Create 4.7.cpp

---
+++ 4.7.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+double factorial(double n)
+{
+    double y = 1;
+    for (int i = 0; i <= n; ++i)
+        y *= i;
+    return y;
+}
+
+float foo(double x, double n)
+{
+    double y = 0;
+    for (double i = 0; i <= n; i++)
+        
+        y += pow(x, i)/factorial(n);
+
+    return y;
+}
+
+int main()
+{
+    double x, n;
+    cout << "Enter x: ";
+    cin >> x;
+    cout << "Enter n: ";
+    cin >> n;
+    cout << foo(x, n);
+    return 0;
+}
+
