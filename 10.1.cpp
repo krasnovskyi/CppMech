28d6d7af34d355a5856bb469d4718e60ebbfc81c
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:13:58 +0300

    Create 10.1.cpp

---
+++ 10.1.cpp
@@ -0,0 +1,76 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Date {
+    unsigned day;
+    unsigned month;
+    int year;
+
+};
+
+
+struct ChessField {
+    char v;
+    unsigned char h;
+
+};
+
+struct Vertex{
+    float x, y;
+
+};
+
+typedef struct {
+    float x, y;     
+
+}   Vertex;
+
+
+
+
+//struct Rectangle{
+//    struct Vertex A;
+//    struct Vertex B;
+
+//}
+
+
+
+
+typedef struct {
+    Vertex A;
+    Vertex B;
+
+} Rectangle;
+
+
+typedef struct {
+    size_t n;
+    double* a; 
+
+}Polynome;
+
+
+int inputRectangle(Rectangle* r) {
+    printf("A(x,y):");
+    scanf("%f %f", &(r->A.x), &(r->A.y));
+    printf("B(x,y):");
+    scanf("%f %f", &(r->B.x), &(r->B.y));
+    return EXIT_SUCCESS;
+
+}
+
+
+void outputRectangle(Rectangle r) {
+    printf("A(%f, %f) - B(%f, %f)", r.A.x, r.A.y, r.B.x, r.B.y);
+
+}
+
+
+int main() {
+    Rectangle r;
+    inputRectangle(&r);
+    outputRectangle(r);
+
+}
