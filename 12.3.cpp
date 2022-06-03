6720268daf2eb38486b7758e2d4ca1ad0fea4d45
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:14:34 +0300

    Create 12.3.cpp

---
+++ 12.3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main3() {
+	int n;
+	float i;
+	cin >> n;
+	cout << fixed;
+	cout << setprecision(2);
+	i = 1.0;
+
+	while (i <= n) {
+		cout << i << "`s square root is " << pow(i, 0.5) << endl;
+		i++;
+	}
+	return 0;
+}
