7267aacb6705a18f386903c983050bbd0a3024d3
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:16:09 +0300

    Create 4.8.cpp

---
+++ 4.8.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <list>
+#include <algorithm>
+using namespace std;
+double foo(int m)
+{
+    
+    double y = 0;
+    for(int i = 0; i<m; ++i)
+        if (pow(4, i) <= m) {
+
+            y = i;
+            
+        }
+    return y;
+}
+
+
+int main()
+{
+    double m;
+    cin >> m;
+    cout << foo(m);
+    return 0;
+}
+
