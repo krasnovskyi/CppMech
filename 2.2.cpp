90dbbb70144067747d8f48f19a414a8b41015a5d
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:14:57 +0300

    Create 2.2.cpp

---
+++ 2.2.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+#include <math.h>
+
+int main() 
+{
+    
+	double a, b, c;
+	printf("a=");
+	scanf_s("%lf", &a);
+	printf("b=");
+	scanf_s("%lf", &b);
+	c = hypot(a, b);
+    printf("c = %0.2f\n", c);
+
+
+}
+
