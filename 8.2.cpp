5746c179835bd87a628164b46619c8cead866acd
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:16:49 +0300

    Create 8.2.cpp

---
+++ 8.2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    double a[3][3] = {
+        {1.0,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+    int x, y;
+    double b;
+    cout << "Enter i: ";
+    cin >> x;
+    cout << "Enter j: ";
+    cin >> y;
+    cout << "Enter a: ";
+    cin >> b;
+    a[x][y] = b;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << '\t' << a[i][j];
+        }
+        cout << endl;
+    }
+
+
+    return 0;
+}
+
