e95bf2f3b2a4e87f2f0c8357ace2aa2a27a83633
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:15:58 +0300

    Create 4.5.cpp

---
+++ 4.5.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+double factorial(int n)
+{
+     int y = 1;
+     if (n % 2 == 0)
+         for (int i = n; i > 0; i--) {
+             if (i % 2 == 0) {
+                 y *= i;
+             }
+         }
+     if (n % 2 != 0)
+         for (int i = n; i > 0; i--) {
+             if (i % 2 != 0) {
+                 y *= i;
+             }
+         }
+     return y;
+    
+        
+   
+}
+int main()
+{
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    cout << factorial(n);
+    return 0;
+}
+
