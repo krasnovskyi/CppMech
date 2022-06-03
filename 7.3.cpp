a43cedfe8a047440655ebe827662edd702b5feb6
krasnovskyi <andriykr12@gmail.com>
2022-06-03 14:16:33 +0300

    Create 7.3.cpp

---
+++ 7.3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    
+    double numbers[10];
+    for (int i = 0; i < 10; i++) {
+        cout << "[" << i + 1 << "]" << ":";
+        cin >> numbers[i];
+        
+    }
+    cout << "Array: ";
+    for (int i = 0; i < 10; i++) {
+        cout << numbers[i] << " ";
+
+    }
+    double s = 0;
+    for (int i = 0; i < 10; i++) {
+        if (numbers[i] < exp(1.0))
+            s += numbers[i];
+ 
+        
+    }
+    cout << "\n" << s;
+    return 0;
+}
+
